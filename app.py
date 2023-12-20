45c8c09bef731ef41678716f8619eba7c140bb36
DianaRodriguez2022 <Diana.rodriguez@otto.de>
2023-12-20 18:24:31 +0100

    uptodate

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS
 import openai
+import json
 
 app = Flask(__name__, template_folder='templates')
-CORS(app)
-# Set up OpenAI API credentials
-openai.api_key = 'keyApi'
+# train chatbot
+train = json.loads(open('venv/train.json').read())
 
+# Set up OpenAI API credentials
+openai.api_key = 'API Key'
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
 
 @app.route('/api', methods=['POST'])
 def ask_chatbot(user_input):
@@ -40,47 +46,3 @@ def get_response():
 
 if __name__ == '__main__':
     app.run()
-#
-#
-#
-#
-#
-#
-#
-#
-# # Define the /api route to handle POST requests
-# @app.route("/api", methods=["POST"])
-# def api():
-#
-#     message = request.form.get("message")
-# #prompt = input("ask me something")
-#
-# # Make an API call to ChatGPT
-# response = openai.Completion.create(
-#     engine="text-davinci-003",  # This engine is known for its versatility and high-quality responses
-#     prompt= message,
-#     #    messages=[
-#     #       {"role": "system", "content": "You are a helpful assistant."},
-#     #      {"role": "user", "content": "Hello!"}
-#     # ],
-#     max_tokens=150
-# )
-
-# print(completion.choices[0].message)
-
-# Get the generated response
-# chatgpt_response = response['choices'][0]['text']
-
-# Print the response
-# print(chatgpt_response)
-
-#   message = request.form.get("message")
-# completion = openai.chatCompletion.created(model="gpt-3.5-turbo", messages=[ {"role": "system", "content": "You are a helpful assistant."},
-#       ]
-#  )
-
-# print(completion.choices[0].message.content)
-# return completion.choices[0].message.content
-
-# if __name__=='__main__':
-#   app.run()
